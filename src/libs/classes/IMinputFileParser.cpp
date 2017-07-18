ffb8632a29ba0a6cb61cc25452750e61db3e27d8
KaiSchu <schueller@aices.rwth-aachen.de>
2017-07-18 12:30:33 +0200

    using *.c_str() instead of a string

--- src/libs/classes/IMinputFileParser.cpp
+++ src/libs/classes/IMinputFileParser.cpp
@@ -53,7 +53,7 @@ IMinputFileParser::IMinputFileParser(string filename){
     
     this->filename=filename;
     string line;
-    ifstream myfile (this->filename);
+    ifstream myfile (this->filename.c_str());
     if (myfile.is_open())
     {
         this->numberOfLines=0;

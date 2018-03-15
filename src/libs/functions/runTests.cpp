432df0449fd4b280364aa19fef4179167113ff83
KaiSchu <schueller@aices.rwth-aachen.de>
2018-03-15 14:24:37 +0100

    change e to E

--- src/libs/functions/runTests.cpp
+++ src/libs/functions/runTests.cpp
@@ -9,7 +9,7 @@
 #include "runTests.hpp"
 
 float isNearlyEqual(float a, float b){
-    return fabsf( a - b ) < 1e-8;
+    return fabsf( a - b ) < 1E-8;
 }
 
 int testerInfo(int condition,string moduleName,int testNumber){
